b4d750a90baf1cb071f37b57ffdf4f0a7d80ef5c
HuotMedia <nicholas@huotmedia.com>
2013-02-28 16:16:41 -0500

    ShowHeloOnFocus plugin to use named helper
    Updated span to use .jquery_form_help_{name} format and leave helper
    spans in dom after fade out. No real need to recreate the dom element
    if the user brings that field back into focus.

--- jquery.formvalidator.js
+++ jquery.formvalidator.js
@@ -44,22 +44,24 @@
                 if(help) {
                     $(this)
                         .focus(function() {
+                        	var $name = $(this).attr('name');
                             var $element = $(this);
-                            if($element.parent().find('.jquery_form_help').length == 0) {
+                            if($element.parent().find('.jquery_form_help_'+$name).length == 0) {
                                 $element.after(
                                       $('<span />')
-                                        .addClass('jquery_form_help')
+                                        .addClass('jquery_form_help_'+$name)
                                         .text(help)
                                         .hide()
                                         .fadeIn()
                                     );
+                            }else{
+                            	$element.parent().find('.jquery_form_help_'+$name).fadeIn();
                             }
                         })
                         .blur(function() {
-                            $(this).parent().find('.jquery_form_help')
-                                .fadeOut('slow', function() {
-                                    $(this).remove();
-                                });
+                        	var $name = $(this).attr('name');
+                            $(this).parent().find('.jquery_form_help_'+$name)
+                                .fadeOut('slow');
                         });
                 }
             });

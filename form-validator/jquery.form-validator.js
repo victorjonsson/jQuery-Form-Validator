a16d4f135e355ad4cc9e84f793273e75c66416d5
Victor Jonsson <kontakt@victorjonsson.se>
2014-07-23 15:02:08 +0200

    One step closer to 2.2.0 release

--- form-validator/jquery.form-validator.js
+++ form-validator/jquery.form-validator.js
@@ -5,7 +5,7 @@
 *
 * @website http://formvalidator.net/
 * @license Dual licensed under the MIT or GPL Version 2 licenses
-* @version 2.1.74
+* @version 2.2.0
 */
 (function($) {
 
--- form-validator/security.dev.js
+++ form-validator/security.dev.js
@@ -13,7 +13,7 @@
  *  - cvv
  *
  * @website http://formvalidator.net/#security-validators
- * @version 2.1.74
+ * @version 2.2.0
  */
 (function($, window) {
 

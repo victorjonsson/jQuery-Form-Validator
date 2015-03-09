400698e4898f0afb7f1a987d1a62d52d65969821
victorjonsson <kontakt@victorjonsson.se>
2015-03-09 08:43:06 +0100

    Minified files, bumped version

--- form-validator/file.dev.js
+++ form-validator/file.dev.js
@@ -10,7 +10,7 @@
  *
  * @website http://formvalidator.net/
  * @license Dual licensed under the MIT or GPL Version 2 licenses
- * @version 2.2.beta.50
+ * @version 2.2.beta.55
  */
 (function($, window) {
 
--- form-validator/security.dev.js
+++ form-validator/security.dev.js
@@ -13,7 +13,7 @@
  *  - cvv
  *
  * @website http://formvalidator.net/#security-validators
- * @version 2.2.beta.50
+ * @version 2.2.beta.55
  */
 (function($, window) {
 

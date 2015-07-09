b516f6fce170ef7a563003f77f1cacc14780b663
Kevin Kelly <caomhainoceallaigh@gmail.com>
2015-07-08 17:18:08 -0700

    Update location.dev.js
    longlat check is passing invalid co-ordinates, updated regex to only pass valid co-ordinates

--- form-validator/location.dev.js
+++ form-validator/location.dev.js
@@ -44,7 +44,7 @@
     $.formUtils.addValidator({
         name : 'longlat',
         validatorFunction : function(str) {
-            var regexp = /^[+-]?\d+\.\d+, ?[+-]?\d+\.\d+$/;
+            var regexp = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
             return regexp.test(str);
         },
         errorMessage:'',

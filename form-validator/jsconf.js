4cfe00a999fe8607f01d9655f8a279d9139b9a1c
Ray <ray-print@users.noreply.github.com>
2016-02-16 00:43:32 -0800

    Bump version number

--- form-validator/jsconf.js
+++ form-validator/jsconf.js
@@ -1,7 +1,7 @@
 /**
  *  JQUERY-FORM-VALIDATOR
  *
- *  @version 2.2.163
+ *  @version 2.2.164
  *  @website http://formvalidator.net/
  *  @author Victor Jonsson, http://victorjonsson.se
  *  @license MIT

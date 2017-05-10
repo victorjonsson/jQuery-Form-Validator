cef586c5f5a65bf5aa4fe19eaa2243847e0a46b1
Victor Jonsson <kontakt@victorjonsson.se>
2017-05-10 22:48:33 +0200

    #320 Added support for age-range in birthday validator

--- src/modules/date.js
+++ src/modules/date.js
@@ -11,72 +11,74 @@
  * @website http://formvalidator.net/#location-validators
  * @license MIT
  */
-(function($) {
+(function ($) {
 
-    $.formUtils.registerLoadedModule('date');
+  $.formUtils.registerLoadedModule('date');
 
-    /*
-     * Validate time hh:mm
-     */
-    $.formUtils.addValidator({
-        name : 'time',
-        validatorFunction : function(time) {
-            if (time.match(/^(\d{2}):(\d{2})$/) === null) {
-                return false;
-            } else {
-                var hours = parseInt(time.split(':')[0],10);
-                var minutes = parseInt(time.split(':')[1],10);
-                if( hours > 23 || minutes > 59 ) {
-                    return false;
-                }
-            }
-            return true;
-        },
-        errorMessage : '',
-        errorMessageKey: 'badTime'
-    });
+  /*
+   * Validate time hh:mm
+   */
+  $.formUtils.addValidator({
+    name: 'time',
+    validatorFunction: function (time) {
+      if (time.match(/^(\d{2}):(\d{2})$/) === null) {
+        return false;
+      } else {
+        var hours = parseInt(time.split(':')[0], 10);
+        var minutes = parseInt(time.split(':')[1], 10);
+        if (hours > 23 || minutes > 59) {
+          return false;
+        }
+      }
+      return true;
+    },
+    errorMessage: '',
+    errorMessageKey: 'badTime'
+  });
 
-    /*
-     * Is this a valid birth date
-     */
-    $.formUtils.addValidator({
-        name : 'birthdate',
-        validatorFunction : function(val, $el, conf) {
-            var dateFormat = 'yyyy-mm-dd';
-            if($el.valAttr('format')) {
-                dateFormat = $el.valAttr('format');
-            }
-            else if(typeof conf.dateFormat !== 'undefined') {
-                dateFormat = conf.dateFormat;
-            }
+  /*
+   * Is this a valid birth date
+   */
+  $.formUtils.addValidator({
+    name: 'birthdate',
+    validatorFunction: function (val, $el, conf) {
+      var dateFormat = 'yyyy-mm-dd';
+      if ($el.valAttr('format')) {
+        dateFormat = $el.valAttr('format');
+      }
+      else if (typeof conf.dateFormat !== 'undefined') {
+        dateFormat = conf.dateFormat;
+      }
 
-            var inputDate = $.formUtils.parseDate(val, dateFormat);
-            if (!inputDate) {
-                return false;
-            }
+      var inputDate = $.formUtils.parseDate(val, dateFormat);
+      if (!inputDate) {
+        return false;
+      }
 
-            var d = new Date();
-            var currentYear = d.getFullYear();
-            var year = inputDate[0];
-            var month = inputDate[1];
-            var day = inputDate[2];
+      var year = inputDate[0],
+        month = inputDate[1],
+        day = inputDate[2],
+        age = getAge(year, month, day),
+        allowedAgeRange = ($el.valAttr('age-range') || '0-124').split('-');
 
-            if (year === currentYear) {
-                var currentMonth = d.getMonth() + 1;
-                if (month === currentMonth) {
-                    var currentDay = d.getDate();
-                    return day <= currentDay;
-                }
-                else {
-                    return month < currentMonth;
-                }
-            }
-            else {
-                return year < currentYear && year > (currentYear - 124); // we can not live for ever yet...
-            }
-        },
-        errorMessage : '',
-        errorMessageKey: 'badDate'
-    });
+      if (allowedAgeRange.length !== 2 || !$.isNumeric(allowedAgeRange[0]) || !$.isNumeric(allowedAgeRange[1])) {
+        throw new Error('Date range format invalid');
+      }
+
+      return age >= allowedAgeRange[0] && age <= allowedAgeRange[1];
+    },
+    errorMessage: '',
+    errorMessageKey: 'badDate'
+  });
+
+
+  function getAge(otherDateYear, otherDateMonth, otherDateDay) {
+    var otherDate = new Date(),
+      nowDate = new Date();
+    otherDate.setYear(otherDateYear);
+    otherDate.setMonth(otherDateMonth);
+    otherDate.setDate(otherDateDay);
+    return new Date(nowDate.getTime() - otherDate.getTime()).getUTCFullYear() - 1970;
+  }
 
 })(jQuery);

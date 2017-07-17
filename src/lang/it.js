ac1be743730e453431ebcd14cf2811e6a62c0575
Filippo <filippo.bisconcin@gmail.com>
2017-07-17 19:32:47 +0200

    IT corrections

--- src/lang/it.js
+++ src/lang/it.js
@@ -28,7 +28,7 @@
       lengthBadEnd: ' caratteri',
       lengthTooLongStart: 'La lunghezza della risposta deve essere minore di ',
       lengthTooShortStart: 'La lunghezza della risposta deve essere maggiore di ',
-      notConfirmed: 'Los valores proporcionados no pudieron ser confirmados',
+      notConfirmed: 'Il valore non è stato confermato.',
       badDomain: 'Il dominio inserito non &egrave; corretto.',
       badUrl: 'L\' URL inserito non &egrave; valido',
       badCustomVal: 'I valori inseriti non sono validi',

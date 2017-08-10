8d04a3088ac8340883a884ac54bc40fe7b615029
Victor Jonsson <kontakt@victorjonsson.se>
2017-08-10 11:02:25 +0200

    #624 fix in age calculation

--- src/modules/date.js
+++ src/modules/date.js
@@ -61,6 +61,8 @@
         age = getAge(year, month, day),
         allowedAgeRange = ($el.valAttr('age-range') || '0-124').split('-');
 
+      $el.trigger('ageCalculated', [age]);
+
       if (allowedAgeRange.length !== 2 || !$.isNumeric(allowedAgeRange[0]) || !$.isNumeric(allowedAgeRange[1])) {
         throw new Error('Date range format invalid');
       }
@@ -71,14 +73,30 @@
     errorMessageKey: 'badDate'
   });
 
-
   function getAge(otherDateYear, otherDateMonth, otherDateDay) {
-    var otherDate = new Date(),
-      nowDate = new Date();
-    otherDate.setYear(otherDateYear);
-    otherDate.setMonth(otherDateMonth);
-    otherDate.setDate(otherDateDay);
-    return new Date(nowDate.getTime() - otherDate.getTime()).getUTCFullYear() - 1970;
+    var birthDate = new Date(otherDateYear, otherDateMonth, otherDateDay), now = new Date(),
+      years = now.getFullYear() - birthDate.getFullYear();
+    birthDate.setFullYear(birthDate.getFullYear() + years);
+    if (birthDate > now) {
+      years--;
+      birthDate.setFullYear(birthDate.getFullYear() - 1);
+    }
+    var days = Math.floor((now.getTime() - birthDate.getTime()) / (3600 * 24 * 1000)),
+      yearsOld = years + days / (isLeapYear(now.getFullYear()) ? 366 : 365),
+      decimals = ((yearsOld + '').split('.')[1] || '').substr(0, 3);
+
+    if (yearsOld >= 0) {
+      return Math.floor(yearsOld) + (decimals >= 915 ? 1:0);
+    } else {
+      decimals *= 10;
+      return Math.floor(yearsOld) + (decimals <= 840 ? 1:0);
+    }
+  }
+
+  function isLeapYear(year) {
+    var d = new Date(year, 1, 28);
+    d.setDate(d.getDate() + 1);
+    return d.getMonth() === 1;
   }
 
 })(jQuery);

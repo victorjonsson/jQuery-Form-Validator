8fb2d14a25eda4b01b1b6795cdc57f627420c301
Damian <kontakt@dszymczuk.pl>
2017-08-20 20:21:59 +0200

    add validator for colors - RGB

--- src/modules/color.js
+++ src/modules/color.js
@@ -45,4 +45,38 @@
     errorMessage: '',
     errorMessageKey: 'badHex'
   });
+
+
+  /**
+   * Check RGB format
+   */
+  $.formUtils.addValidator({
+    name: 'rgb',
+    validatorFunction: function(val, $el){
+      if($el.valAttr('allow-transparent') === 'true' && val === 'transparent'){
+        return true;
+      };
+
+      var removedSpace = val.replace(/ /g, '');
+      var regex = /\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)/i;
+
+      if(removedSpace.match(regex)){
+        var removeBrackets = removedSpace.replace(/\(/g, '').replace(/\)/g, '');
+        var valueSliced = removeBrackets.split(',');
+        var isValid = true;
+
+        valueSliced.forEach(function(i){
+          var parsedInt = parseInt(i);
+          if((Number.isInteger(parsedInt) && 0 <= parsedInt && parsedInt <= 255) === false)
+            isValid = false;
+        });
+        return isValid;
+      }
+
+      return false;
+    },
+    errorMessage: '',
+    errorMessageKey: 'badRgb'
+  });
+
 })(jQuery);

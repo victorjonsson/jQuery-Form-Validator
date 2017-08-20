b9346091a4b22b32a9783d193d571ae87a1d55ff
Damian <kontakt@dszymczuk.pl>
2017-08-20 20:23:37 +0200

    add validator for colors - RGBA

--- src/modules/color.js
+++ src/modules/color.js
@@ -16,6 +16,20 @@
 
   $.formUtils.registerLoadedModule('color');
 
+  /*
+    HELPER FUNCTIONS
+   */
+  var filterFloat = function(value) {
+    if (/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
+        .test(value))
+      return Number(value);
+    return NaN;
+  };
+
+  var isBetween0and1 = function(value){
+    return value > 0 && value < 1;
+  };
+
   /**
    * Check HEX format
    */
@@ -77,6 +91,45 @@
     },
     errorMessage: '',
     errorMessageKey: 'badRgb'
+  });
+
+  $.formUtils.addValidator({
+    name: 'rgba',
+    validatorFunction: function(val, $el, config, language){
+      if($el.valAttr('allow-transparent') === 'true' && val === 'transparent'){
+        return true;
+      };
+
+      var removedSpace = val.replace(/ /g, '');
+      var regex = /\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3},[0,1]{1}.?[0-9]*\)/i;
+
+      if(removedSpace.match(regex)){
+        var removeBrackets = removedSpace.replace(/\(/g, '').replace(/\)/g, '');
+        var valueSliced = removeBrackets.split(',');
+        var isValid = true;
+
+        valueSliced.forEach(function(i,index){
+          var value = filterFloat(i);
+          if(Number.isInteger(value)){
+            var isInRange = value >= 0 && value <= 255;
+            if(!isInRange){
+              isValid = false;
+            }
+          } else {
+            if(!isBetween0and1(value)){
+              isValid = false;
+            }
+          }
+        });
+        return isValid;
+      } else {
+        console.log("Bad regex");
+      }
+
+      return false;
+    },
+    errorMessage: '',
+    errorMessageKey: 'badRgba'
   });
 
 })(jQuery);
